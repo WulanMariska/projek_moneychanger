132ec6a84f7dc0101de297750ff47f77129a4a71
Wulan Mariska <wulanmariska436@gmail.com>
2026-08-28 19:55:19 +0700

    feat: add money changer welcome route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route :: get('/ money-changer', function ()  {
+    return 'Selamat Datang di Money Changer!';
+});
